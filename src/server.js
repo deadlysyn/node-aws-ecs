3ae3e7e8c00e32b31a07f8cec27993038936d068
Mike Hoskins <mrh@devopsdreams.io>
2020-04-30 21:44:35 -0400

    update to test secret retrieval

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const express = require('express')
 
 const HOST = process.env.LISTEN_HOST
 const PORT = process.env.LISTEN_PORT
+const SECRET = process.env.TEST_SECRET
 
 const app = express()
 
 app.get('/', (req, res) => {
   console.log(`Said hello to ${req.ip}`)
-  res.send('Hello World')
+  // Example secret retrieval, obviously in the real world you
+  // would never put secrets in public places!
+  res.send(`Top secret message: ${SECRET}`)
 });
 
 app.listen(PORT, HOST)
